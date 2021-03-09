869bf57836aebdef89c422c1db4c33935c600445
Gabriel Toledo <gtoledo@ice.ufjf.br>
2021-03-09 19:09:39 -0300

    Controle de play e pause

--- js/Main.js
+++ js/Main.js
@@ -7,13 +7,23 @@ const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas);
 cena1.desenhar();
 
-const pc = new Sprite({});
+const pc = new Sprite({vx: 10});
 const en1 = new Sprite({x:140, w:30, color:"red"});
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
 
+cena1.iniciar(0);
 
-cena1.quadro(0);
+document.addEventListener("keydown", (e)=>{
+    switch (e.key) {
+        case "s":
+            cena1.iniciar();
+            break;
+        case "S":
+            cena1.parar();
+            break;   
+    }
+})
 
 
