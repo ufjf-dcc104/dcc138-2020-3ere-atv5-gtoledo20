e3fe632bb6b22dc88ee79547d8d2dcb073862206
Gabriel Toledo <gtoledo@ice.ufjf.br>
2021-03-09 20:19:21 -0300

    Cria arquivo de gerenciador de recursos

--- js/Main.js
+++ js/Main.js
@@ -1,23 +1,38 @@
+import AssetManager from "./AssetManager.js";
 import Cena from "./Cena.js";
 import Sprite from "./Sprite.js";
 
+const img1= new Image();
+img1.src="assets/garota.png";
+const img2= new Image();
+img2.src="assets/orc.png";
+const img3= new Image();
+img3.src="assets/skelly.png";
+
+document.body.appendChild(img1);
+document.body.appendChild(img2);
+document.body.appendChild(img3);
+
+const assets = new AssetManager();
+
 const canvas = document.querySelector("canvas");
-const cena1 = new Cena(canvas);
-const pc = new Sprite({vx: 10});
-const en1 = new Sprite({x:140, h:30, color:"red"});
+const cena1 = new Cena(canvas, assets);
+const pc = new Sprite({ vx: 10 });
+const en1 = new Sprite({ x: 140, h: 30, color: "red" });
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
-cena1.adicionar(new Sprite({y:40, h:30, color:"red"}));
+cena1.adicionar(new Sprite({ y: 40, h: 30, color: "red" }));
 
 cena1.iniciar();
 
-document.addEventListener("keydown", (e)=>{
-    switch (e.key) {
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
     case "s":
-        cena1.iniciar();
-        break;
+      cena1.iniciar();
+      break;
     case "S":
-        cena1.parar();
-        break;
-}})
+      cena1.parar();
+      break;
+  }
+});
