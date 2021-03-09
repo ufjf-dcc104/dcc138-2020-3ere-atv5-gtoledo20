fb238963ba250338792684bb0f02f6a4c5ae3ec1
Gabriel Toledo <gtoledo@ice.ufjf.br>
2021-03-09 17:50:00 -0300

    Cria e usa uma classe de cena

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,8 @@
+import Cena from "./Cena.js"
+/*  É responsável por desenhar elementos na tela em uma animação
+*/
 console.log("Hello, World!");
 const canvas = document.querySelector("canvas");
 console.log(canvas);
+const cena1 = new Cena(canvas);
+cena1.desenhar();
