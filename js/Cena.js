ff8cf6cf42832f57dd50f7046d1c07adb979b151
Gabriel Toledo <gtoledo@ice.ufjf.br>
2021-03-09 19:37:54 -0300

    Adiciona teste de colisao de sprites

--- js/Cena.js
+++ js/Cena.js
@@ -1,4 +1,7 @@
 export default class Cena {
+    /*
+    É responsável por desenhar elementos na tela em uma animação.
+    */
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
@@ -8,15 +11,15 @@ export default class Cena {
         this.idAnim = null;
     }
     desenhar(){
-        this.ctx.fillStyle = "gray";
-        this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle = "grey";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         for (let s = 0; s < this.sprites.length; s++) {
             const sprite = this.sprites[s];
             sprite.desenhar(this.ctx);
         }
     }
-    adicionar(sprite){
-        this.sprites.push(sprite);
+    adicionar(sprites){
+        this.sprites.push(sprites);
     }
     passo(dt){
         for (const sprite of this.sprites) {
@@ -30,19 +33,28 @@ export default class Cena {
 
         this.passo(this.dt);
         this.desenhar();
+        this.checaColisao();
 
         this.iniciar();
-
         this.t0 = t;
     }
     iniciar(){
-        this.idAnim = requestAnimationFrame(
-            (t)=>{ this.quadro(t);}
-        );
+        this.idAnim = requestAnimationFrame((t) => {this.quadro(t);});
     }
     parar(){
         cancelAnimationFrame(this.idAnim);
         this.t0 = null;
         this.dt = 0;
+    }
+    checaColisao(){
+        for (let a = 0; a < this.sprites.length - 1; a++) {
+            const spriteA = this.sprites[a];
+            for (let b = a+1; b < this.sprites.length; b++) {
+                const spriteB = this.sprites[b];
+                if(spriteA.colidiuCom(spriteB)){
+                console.log(spriteA, spriteB);
+                }
+            }
+        }
     }
 }
